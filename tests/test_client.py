8cc9ffc9c7a830760d4da660c63c1bebf5203a8a
scottcountry <scottp@incountry.com>
2019-07-18 15:30:09 -0700

    Updated README with country paths

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,7 +5,7 @@ import uuid
 @pytest.fixture()
 def client():
 	#zone_id=ZONE_ID, api_key=API_KEY, host=HOST)
-	c = incountry.Storage(encrypt=False)
+	c = incountry.Storage(encrypt=False, use_ssl=False)
 	yield c
 
 def test_write_single_pop(client):
@@ -28,28 +28,38 @@ def test_read_single_pop(client):
 	r = client.read(country='IT', key="record2")
 	assert r['body'] == "Welcome to Rome"
 
-def test_delete_single_pop(client):
-	client.write(country='ruc1', key='record1', body='Konichiwa')
-	r = client.read(country='ruc1', key='record1')
-	assert r is not None
+def test_read_single_pop(client):
+	client.write(country='mv', key="record1", body="Welcome to Japan")
+	client.write(country='jp', key="record2", body="Welcome again to Japan")
 
-	client.delete(country='ruc1', key='record1')
+	r = client.read(country='mv', key="record1")
+	assert r['body'] == "Welcome to Japan"
 
-	r = client.read(country='ruc1', key='record1')
-	assert r is None
+	r = client.read(country='jp', key="record2")
+	assert r['body'] == "Welcome again to Japan"
 
-def test_using_encryption():
-	eclient = incountry.Storage(encrypt=True, use_ssl=True)
-	eclient.write(country="ru", key="key1", body="You cant read this text")
+# def test_delete_single_pop(client):
+# 	client.write(country='ruc1', key='record1', body='Konichiwa')
+# 	r = client.read(country='ruc1', key='record1')
+# 	assert r is not None
 
-	client = incountry.Storage(encrypt=False, use_ssl=True)
-	# Keys won't clash because of encryption
-	client.write(country="ru", key="key1", body="You CAN read this text")
+# 	client.delete(country='ruc1', key='record1')
 
-	r = eclient.read(country="ru", key="key1")
-	assert r['body'] == "You cant read this text"
+# 	r = client.read(country='ruc1', key='record1')
+# 	assert r is None
 
-	r = client.read(country="ru", key="key1")
-	assert r['body'] == "You CAN read this text"
+# def test_using_encryption():
+# 	eclient = incountry.Storage(encrypt=True, use_ssl=True)
+# 	eclient.write(country="ru", key="key1", body="You cant read this text")
+
+# 	client = incountry.Storage(encrypt=False, use_ssl=True)
+# 	# Keys won't clash because of encryption
+# 	client.write(country="ru", key="key1", body="You CAN read this text")
+
+# 	r = eclient.read(country="ru", key="key1")
+# 	assert r['body'] == "You cant read this text"
+
+# 	r = client.read(country="ru", key="key1")
+# 	assert r['body'] == "You CAN read this text"
 
 
