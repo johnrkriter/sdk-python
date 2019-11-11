881d6b7dde5c6a4bbe7ac48c25f10c4deccee8d0
Alexander Shlygin <alexander.shlygin@incountry.com>
2019-11-11 14:48:36 +0400

    [DEVOPS-718] Add Dependency-Track with Travis

---
+++ dependencytrack_upload.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env sh
+
+# common variables
+API_KEY=$1
+API_URL=$2
+VERSION=$4
+PROJECT_NAME=$3
+PROJECT_ID=`curl -s -X "GET" "${API_URL}/api/v1/project" \
+     -H 'Content-Type: application/json' \
+     -H "X-API-Key: ${API_KEY}" | jq -j ".[]?
+                  | select(.version == \"${VERSION}\" and .name == \"${PROJECT_NAME}\")
+                  | .uuid"`
+
+echo "Uploading BOM for project ${PROJECT_NAME} version ${VERSION}"
+
+if [ -z "${PROJECT_ID}" ]; then
+  echo "Adding project ${PROJECT_NAME} with ${VERSION} to Dependency-Track"
+  PROJECT_ID=`curl -s -X "PUT" "${API_URL}/api/v1/project" \
+     -H 'Content-Type: application/json' \
+     -H "X-API-Key: ${API_KEY}" \
+     -d "{
+       \"name\": \"${PROJECT_NAME}\",
+       \"version\": \"${VERSION}\"
+     }" | jq -j '.uuid'`
+fi
+
+[ -z "${PROJECT_ID}" ] && echo "Can't create project ${PROJECT_NAME} with version ${VERSION}" && exit 1
+
+# convert Pipfile.lock to requirements.txt with jq
+jq -r '.default
+        | to_entries[]
+        | .key + .value.version' \
+    Pipfile.lock > requirements.txt
+
+# generate bom.xml
+cyclonedx-py
+
+echo "Adding BOM for PROJECT with ID ${PROJECT_ID}..."
+
+printf "{ \"project\": \"${PROJECT_ID}\", \"bom\": \"" > payload.json
+base64 -w0 bom.xml >> payload.json
+printf "\" }\n" >> payload.json
+
+RESULT=`curl -s -X "PUT" "${API_URL}/api/v1/bom" \
+     -H 'Content-Type: application/json' \
+     -H "X-API-Key: ${API_KEY}" \
+     -d @payload.json \
+     -o /dev/null -w "%{http_code}"`
+
+echo "Result code is ${RESULT}"
+[ "$RESULT" = "200" ] || (echo "Can't post BOM to Dependency-Track" && exit 1)
