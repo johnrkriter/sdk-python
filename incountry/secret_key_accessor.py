fc5acd7b2518d4d5aa308e600ef88db6816a18ab
Rommero <rommero.semin@gmail.com>
2019-11-19 18:09:41 +0300

    EN-1586 [ENH] Add support for multiple keys in SecretKeyAccessor [ENH] Add key version management for storage/crypto [ENH] add jsonschema validation for SecretKeyAccessor callback result [ENH] add jsonschema to deps [ENH] update tests to accomodate latest changes

--- incountry/secret_key_accessor.py
+++ incountry/secret_key_accessor.py
@@ -1,11 +1,43 @@
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
 from .exceptions import SecretKeyAccessorException
+
+from .validation import secret_key_accessor_response_schema
 
 
 class SecretKeyAccessor:
+    DEFAULT_VERSION = 0
+
     def __init__(self, accessor_function):
         if not callable(accessor_function):
             raise SecretKeyAccessorException("Argument accessor_function must be a function")
         self._accessor_function = accessor_function
 
-    def get_secret(self):
-        return self._accessor_function()
+    def get_key(self, key_version=None):
+        if key_version is not None and not isinstance(key_version, int):
+            raise SecretKeyAccessorException(
+                "Invalid secret key version requested. Version should be of type `int`"
+            )
+
+        keys_data = self._accessor_function()
+
+        if isinstance(keys_data, str):
+            return [keys_data, SecretKeyAccessor.DEFAULT_VERSION]
+
+        try:
+            validate(instance=keys_data, schema=secret_key_accessor_response_schema)
+        except ValidationError as e:
+            raise SecretKeyAccessorException("SecretKeyAccessor validation error") from e
+
+        version_to_search = (
+            key_version if key_version is not None else keys_data.get("currentKeyVersion")
+        )
+
+        for key_data in keys_data.get("keys"):
+            if key_data.get("keyVersion") == version_to_search:
+                return [key_data.get("key"), version_to_search]
+
+        raise SecretKeyAccessorException(
+            "Secret key not found for version {}".format(version_to_search)
+        )
--- tests/unit/test_secrey_key_accessor.py
+++ tests/unit/test_secrey_key_accessor.py
@@ -7,11 +7,75 @@ from incountry import SecretKeyAccessor, SecretKeyAccessorException
 
 @pytest.mark.parametrize("password", ["password"])
 @pytest.mark.happy_path
-def test_get_secret(password):
+def test_get_key_old(password):
     secret_accessor = SecretKeyAccessor(lambda: password)
-    assert secret_accessor.get_secret() == password
+    [secret, key_version] = secret_accessor.get_key()
+    assert secret == password
+    assert key_version == SecretKeyAccessor.DEFAULT_VERSION
+
+
+@pytest.mark.parametrize(
+    "keys_data, proper_version, proper_key",
+    [
+        (
+            {
+                "currentKeyVersion": 1,
+                "keys": [
+                    {"key": "password0", "keyVersion": 0},
+                    {"key": "password1", "keyVersion": 1},
+                ],
+            },
+            1,
+            "password1",
+        )
+    ],
+)
+@pytest.mark.happy_path
+def test_get_key(keys_data, proper_version, proper_key):
+    secret_accessor = SecretKeyAccessor(lambda: keys_data)
+    [secret, key_version] = secret_accessor.get_key()
+    assert key_version == proper_version
+    assert secret == proper_key
 
 
 @pytest.mark.error_path
 def test_non_callable_accessor_function():
     SecretKeyAccessor.when.called_with("password").should.have.raised(SecretKeyAccessorException)
+
+
+@pytest.mark.error_path
+def test_incorrect_version_requested():
+    secret_accessor = SecretKeyAccessor(lambda: "some password")
+    secret_accessor.get_key.when.called_with("non int").should.have.raised(
+        SecretKeyAccessorException
+    )
+
+
+@pytest.mark.parametrize(
+    "keys_data", [{"currentKeyVersion": 1, "keys": [{"key": "password", "keyVersion": 1}]}]
+)
+@pytest.mark.error_path
+def test_non_existing_version_requested(keys_data):
+    secret_accessor = SecretKeyAccessor(lambda: keys_data)
+    secret_accessor.get_key.when.called_with(key_version=0).should.have.raised(
+        SecretKeyAccessorException
+    )
+
+
+@pytest.mark.parametrize(
+    "keys_data",
+    [
+        {"keys": [{"key": "password", "keyVersion": 1}]},
+        {"currentKeyVersion": 1},
+        {"currentKeyVersion": "1", "keys": [{"key": "password", "keyVersion": 1}]},
+        {"currentKeyVersion": 1, "keys": [{"key": "password", "keyVersion": "1"}]},
+        {"currentKeyVersion": 1, "keys": [{"key": 1, "keyVersion": 1}]},
+        {"currentKeyVersion": 1, "keys": []},
+    ],
+)
+@pytest.mark.error_path
+def test_invalid_keys_object(keys_data):
+    secret_accessor = SecretKeyAccessor(lambda: keys_data)
+    secret_accessor.get_key.when.called_with(key_version=0).should.have.raised(
+        SecretKeyAccessorException
+    )
