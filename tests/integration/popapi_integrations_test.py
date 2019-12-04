ae2662493a2fe085f0cd761ced1b1407d4659926
Tony <anton.sivogrivov@incountry.com>
2019-12-04 15:46:39 +0300

    EN-1491 code style

--- tests/integration/popapi_integrations_test.py
+++ tests/integration/popapi_integrations_test.py
@@ -3,18 +3,16 @@ from pytest_testrail.plugin import pytestrail
 from incountry import SecretKeyAccessor
 import uuid
 
-API_KEY = 'fjoxhg.7ea9257f8fe54899b8e8136c62fd02a3'
-ENVIRONMENT_ID = 'b6517bec-2a52-4730-aa50-a90e2272bb2f'
-ENDPOINT = 'https://us.staging.incountry.io'
+API_KEY = "fjoxhg.7ea9257f8fe54899b8e8136c62fd02a3"
+ENVIRONMENT_ID = "b6517bec-2a52-4730-aa50-a90e2272bb2f"
+ENDPOINT = "https://us.staging.incountry.io"
 SECRETS_DATA = {
-    'secrets': [{
-        'secret': 'supersecret',
-        'version': 2,
-    }],
-    'currentVersion': 2,
+    "secrets": [{"secret": "supersecret", "version": 2,}],
+    "currentVersion": 2,
 }
 
-@pytestrail.case('C149')
+
+@pytestrail.case("C149")
 def test_e2e():
     client = Storage(
         api_key=API_KEY,
@@ -22,33 +20,25 @@ def test_e2e():
         endpoint=ENDPOINT,
         encrypt=True,
         secret_key_accessor=SecretKeyAccessor(lambda: SECRETS_DATA),
-        debug=True
+        debug=True,
     )
 
     # This pattern will be useful for parameterized tests when we are ready for them
     test_case = {
-        'country': 'us',
-        'key': uuid.uuid4().hex,
-        'body': 'test',
+        "country": "us",
+        "key": uuid.uuid4().hex,
+        "body": "test",
     }
 
     write_response = client.write(
-        country=test_case['country'],
-        key=test_case['key'],
-        body=test_case['body'],
+        country=test_case["country"], key=test_case["key"], body=test_case["body"],
     )
 
-    read_response = client.read(
-        country=test_case['country'],
-        key=test_case['key']
-    )
+    read_response = client.read(country=test_case["country"], key=test_case["key"])
 
     assert read_response is not None
-    assert read_response['body'] == test_case['body']
-    assert read_response['key'] == test_case['key']
-    assert read_response['version'] == 2
+    assert read_response["body"] == test_case["body"]
+    assert read_response["key"] == test_case["key"]
+    assert read_response["version"] == 2
 
-    delete_response = client.delete(
-        country=test_case['country'],
-        key=test_case['key']
-    )
+    delete_response = client.delete(country=test_case["country"], key=test_case["key"])
--- tests/integration/test_storage.py
+++ tests/integration/test_storage.py
@@ -4,16 +4,14 @@ import uuid
 from pytest_testrail.plugin import pytestrail
 from incountry import SecretKeyAccessor
 
-API_KEY = 'fjoxhg.7ea9257f8fe54899b8e8136c62fd02a3'
-ENVIRONMENT_ID = 'b6517bec-2a52-4730-aa50-a90e2272bb2f'
-ENDPOINT = 'https://us.staging.incountry.io'
+API_KEY = "fjoxhg.7ea9257f8fe54899b8e8136c62fd02a3"
+ENVIRONMENT_ID = "b6517bec-2a52-4730-aa50-a90e2272bb2f"
+ENDPOINT = "https://us.staging.incountry.io"
 SECRETS_DATA = {
-    'secrets': [{
-        'secret': 'supersecret',
-        'version': 2,
-    }],
-    'currentVersion': 2,
+    "secrets": [{"secret": "supersecret", "version": 2, }],
+    "currentVersion": 2,
 }
+
 
 @pytest.fixture()
 def client():
@@ -26,71 +24,67 @@ def client():
         endpoint=ENDPOINT,
     )
 
-    
-@pytestrail.case('C143')
+
+@pytestrail.case("C143")
 def test_write_single_pop(client):
     key1 = uuid.uuid4().hex
-    client.write(country='us', key=key1)
-    r = client.read(country='us', key=key1)
+    client.write(country="us", key=key1)
+    r = client.read(country="us", key=key1)
     assert r is not None
-    assert 'version' in r
-    assert r['key'] == key1
+    assert "version" in r
+    assert r["key"] == key1
 
 
-@pytestrail.case('C144')
+@pytestrail.case("C144")
 def test_read_single_pop(client):
     key1 = uuid.uuid4().hex
     key2 = uuid.uuid4().hex
-    
-    client.write(country='us', key=key1, body='Welcome to Florence')
-    client.write(country='us', key=key2, body='Welcome to Rome')
 
-    r = client.read(country='us', key=key1)
-    assert r['body'] == 'Welcome to Florence'
+    client.write(country="us", key=key1, body="Welcome to Florence")
+    client.write(country="us", key=key2, body="Welcome to Rome")
 
-    r = client.read(country='us', key=key2)
-    assert r['body'] == 'Welcome to Rome'
+    r = client.read(country="us", key=key1)
+    assert r["body"] == "Welcome to Florence"
+
+    r = client.read(country="us", key=key2)
+    assert r["body"] == "Welcome to Rome"
 
 
-@pytestrail.case('C147')
+@pytestrail.case("C147")
 def test_delete_single_pop(client):
     key1 = uuid.uuid4().hex
-    
-    client.write(country='us', key=key1, body='Konichiwa')
-    r = client.read(country='us', key=key1)
+
+    client.write(country="us", key=key1, body="Konichiwa")
+    r = client.read(country="us", key=key1)
     assert r is not None
 
-    client.delete(country='us', key=key1)
+    client.delete(country="us", key=key1)
 
-    r = client.read(country='us', key=key1)
+    r = client.read(country="us", key=key1)
     assert r is None
 
 
-@pytestrail.case('C148')
+@pytestrail.case("C148")
 def test_using_encryption():
     key1 = uuid.uuid4().hex
-    
+
     eclient = incountry.Storage(
         api_key=API_KEY,
         environment_id=ENVIRONMENT_ID,
         endpoint=ENDPOINT,
-        encrypt=True, 
-        secret_key_accessor=SecretKeyAccessor(lambda: SECRETS_DATA)
+        encrypt=True,
+        secret_key_accessor=SecretKeyAccessor(lambda: SECRETS_DATA),
     )
-    eclient.write(country='us', key=key1, body='You cant read this text')
+    eclient.write(country="us", key=key1, body="You cant read this text")
 
     client = incountry.Storage(
-        api_key=API_KEY,
-        environment_id=ENVIRONMENT_ID,
-        endpoint=ENDPOINT,
-        encrypt=False
+        api_key=API_KEY, environment_id=ENVIRONMENT_ID, endpoint=ENDPOINT, encrypt=False
     )
     # Keys won't clash because of encryption
-    client.write(country='us', key=key1, body='You CAN read this text')
+    client.write(country="us", key=key1, body="You CAN read this text")
 
-    r = eclient.read(country='us', key=key1)
-    assert r['body'] == 'You cant read this text'
+    r = eclient.read(country="us", key=key1)
+    assert r["body"] == "You cant read this text"
 
-    r = client.read(country='us', key=key1)
-    assert r['body'] == 'You CAN read this text'
-
+    r = client.read(country="us", key=key1)
+    assert r["body"] == "You CAN read this text"
